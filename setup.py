4e9040de94a48f8d6fbec6fb99cbb3bb5661363d
Jonas Baumann <jone@jone.ch>
2013-05-11 13:05:40 +0200

    Bootstrap package.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+
+version = '1.0'
+
+
+tests_require = [
+    'unittest2',
+    'mocker',
+    'nose',
+    ]
+
+
+setup(name='github-watchlist',
+      version=version,
+      description='Manage your watched GitHub repositories per script',
+      long_description=open('README.rst').read(),
+
+      classifiers=[
+        'Intended Audience :: Developers',
+        ],
+
+      keywords='github watchlist',
+      author='Jonas Baumann',
+      url='https://github.com/jone/github-watchlist',
+
+      license='Beerware',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'pygithub3',
+        ],
+
+      tests_require=tests_require,
+      extras_require={'tests': tests_require},
+      )
