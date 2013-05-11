d56551fce461b93485a7a134fd3d1abcc99e5e9c
Jonas Baumann <jone@jone.ch>
2013-05-12 00:15:49 +0200

    Implement config parser.

---
+++ watchlist/config.py
@@ -0,0 +1,67 @@
+import ConfigParser
+import re
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+class Config(object):
+
+    def load(self, path):
+        config = self.read_config_file(path)
+        self.validate_config(config)
+        self.apply_config(config)
+
+    def read_config_file(self, path):
+        config = ConfigParser.ConfigParser()
+
+        with open(path) as configfile:
+            try:
+                config.readfp(configfile)
+            except ConfigParser.Error, exc:
+                raise ConfigurationError('%s: %s' % (type(exc).__name__, str(exc)))
+
+        return config
+
+    def validate_config(self, config):
+        sections = config.sections()
+        if sections != ['watchlist']:
+            raise ConfigurationError(
+                'The config file should have exactly one section called "watchlist",'
+                ' got: %s' % (sections and ', '.join(sorted(config.sections()))
+                              or 'none'))
+
+        required_options = ('github-login', 'github-oauth-token', 'watchlist')
+        missing_options = set(required_options) - set(config.options('watchlist'))
+        if len(missing_options) > 0:
+            raise ConfigurationError('Missing configuration options: %s' % (
+                    ', '.join(sorted(missing_options))))
+
+        empty_options = [name for name in ('github-login', 'github-oauth-token')
+                         if not config.get('watchlist', name)]
+        if len(empty_options) > 0:
+            raise ConfigurationError('Values required for the options %s.' % (
+                    ' and '.join(empty_options)))
+
+        unexpected_options = set(config.options('watchlist')) - set(required_options)
+        if len(unexpected_options) > 0:
+            raise ConfigurationError(
+                'Unexpected configuration options: %s.' % (
+                    ', '.join(sorted(unexpected_options))) +
+                ' Is the watchlist not properly indented?')
+
+    def apply_config(self, config):
+        self.github_login = config.get('watchlist', 'github-login')
+        self.github_oauth_token = config.get('watchlist', 'github-oauth-token')
+
+        self.watchlist = []
+        watchlist_value = config.get('watchlist', 'watchlist')
+
+        item_xpr = re.compile('^(watching|not-watching|ignoring): *(\S*)$')
+        for line in map(str.strip, watchlist_value.strip().split('\n')):
+            match = item_xpr.match(line)
+            if not match:
+                raise ConfigurationError('Unexpected format of watchlist item: %s' % (
+                        line))
+            self.watchlist.append(match.groups())
---
+++ watchlist/tests/test_config.py
@@ -0,0 +1,135 @@
+from unittest2 import TestCase
+from tempfile import NamedTemporaryFile
+from watchlist.config import Config
+from watchlist.config import ConfigurationError
+
+
+class TestConfig(TestCase):
+
+    def test_parsing_errors_are_reported(self):
+        with NamedTemporaryFile() as configfile:
+            configfile.write('\n'.join((
+                        '[watchlist',)))
+            configfile.file.flush()
+
+            with self.assertRaises(ConfigurationError) as cm:
+                Config().load(configfile.name)
+
+            self.assertIn(
+                'MissingSectionHeaderError: File contains no section headers.',
+                str(cm.exception))
+
+    def test_configuration_error_when_no_section_in_config_file(self):
+        with NamedTemporaryFile() as configfile:
+            # empty config file
+
+            with self.assertRaises(ConfigurationError) as cm:
+                Config().load(configfile.name)
+
+            self.assertEquals(
+                'The config file should have exactly one section called "watchlist",'
+                ' got: none', str(cm.exception))
+
+    def test_configuration_error_when_wrong_sections_in_config_file(self):
+        with NamedTemporaryFile() as configfile:
+            configfile.write('\n'.join((
+                        '[foo]',
+                        '[bar]')))
+            configfile.file.flush()
+
+            with self.assertRaises(ConfigurationError) as cm:
+                Config().load(configfile.name)
+
+            self.assertEquals(
+                'The config file should have exactly one section called "watchlist",'
+                ' got: bar, foo', str(cm.exception))
+
+    def test_configuration_error_when_missing_configuration_options(self):
+        with NamedTemporaryFile() as configfile:
+            configfile.write('\n'.join((
+                        '[watchlist]',
+                        '')))
+            configfile.file.flush()
+
+            with self.assertRaises(ConfigurationError) as cm:
+                Config().load(configfile.name)
+
+            self.assertEquals(
+                'Missing configuration options: github-login, github-oauth-token,'
+                ' watchlist', str(cm.exception))
+
+    def test_configuration_error_on_bad_indent_in_watchlist(self):
+        with NamedTemporaryFile() as configfile:
+            configfile.write('\n'.join((
+                        '[watchlist]',
+                        'github-login = foo',
+                        'github-oauth-token = bar',
+                        'watchlist =',
+                        'watching: foo/*')))
+            configfile.file.flush()
+
+            with self.assertRaises(ConfigurationError) as cm:
+                Config().load(configfile.name)
+
+            self.assertEquals(
+                'Unexpected configuration options: watching.'
+                ' Is the watchlist not properly indented?', str(cm.exception))
+
+    def test_login_and_token_value_required(self):
+        with NamedTemporaryFile() as configfile:
+            configfile.write('\n'.join((
+                        '[watchlist]',
+                        'github-login =',
+                        'github-oauth-token =',
+                        'watchlist =')))
+            configfile.file.flush()
+
+            with self.assertRaises(ConfigurationError) as cm:
+                Config().load(configfile.name)
+
+            self.assertEquals(
+                'Values required for the options github-login and github-oauth-token.',
+                str(cm.exception))
+
+    def test_propper_config_has_values(self):
+        config = Config()
+
+        with NamedTemporaryFile() as configfile:
+            configfile.write('\n'.join((
+                        '[watchlist]',
+                        'github-login = foo',
+                        'github-oauth-token = bar',
+                        'watchlist =',
+                        '  watching: foo/*',
+                        '  watching: bar/bar',
+                        '  ignoring: bar/*')))
+            configfile.file.flush()
+            config.load(configfile.name)
+
+        self.assertEquals('foo', config.github_login, 'Wrong github login')
+        self.assertEquals('bar', config.github_oauth_token, 'Wrong github token')
+
+        self.assertEquals(
+            [('watching', 'foo/*'),
+             ('watching', 'bar/bar'),
+             ('ignoring', 'bar/*')],
+            config.watchlist, 'Unexpected watchlist in config')
+
+    def test_configuration_error_when_watchlist_item_is_invalid(self):
+        with NamedTemporaryFile() as configfile:
+            configfile.write('\n'.join((
+                        '[watchlist]',
+                        'github-login = foo',
+                        'github-oauth-token = bar',
+                        'watchlist =',
+                        '  watching: foo/*',
+                        '  bar/bar',
+                        '  ignoring: bar/*')))
+            configfile.file.flush()
+
+            with self.assertRaises(ConfigurationError) as cm:
+                Config().load(configfile.name)
+
+            self.assertEquals(
+                'Unexpected format of watchlist item: bar/bar',
+                str(cm.exception))
