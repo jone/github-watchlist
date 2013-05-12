230bcf4e9d8dab9ded3e6c7a768e959220bad216
Jonas Baumann <jone@jone.ch>
2013-05-12 12:35:57 +0200

    Split up subscriptions module in loader and strategy.

--- watchlist/strategy.py
+++ watchlist/strategy.py
@@ -1,33 +1,4 @@
-from watchlist import github
 import re
-
-
-class CurrentSubscriptionsLoader(object):
-
-    def __init__(self, config):
-        self.config = config
-
-    def load_current_subscriptions(self):
-        repositories = {}
-        watching = self.get_watching_repositories()
-
-        for reponame in self.get_member_repositories():
-            repositories[reponame] = {'watching': reponame in watching}
-
-        return repositories
-
-    def get_watching_repositories(self):
-        response = github.get('user/subscriptions', self.config)
-        return set(map(lambda repo: repo.get('full_name'), response))
-
-    def get_member_repositories(self):
-        repositories = github.get('user/repos', self.config)
-
-        for organisation in github.get('user/orgs', self.config):
-            org_name = organisation.get('login')
-            repositories.extend(github.get('orgs/%s/repos' % org_name, self.config))
-
-        return set(map(lambda repo: repo.get('full_name'), repositories))
 
 
 class UpdateStrategy(object):
--- watchlist/tests/test_strategy.py
+++ watchlist/tests/test_strategy.py
@@ -1,35 +1,6 @@
-from unittest2 import TestCase
-from watchlist.subscriptions import CurrentSubscriptionsLoader
-from watchlist.subscriptions import UpdateStrategy
-from watchlist.tests.base import GithubMockTestCase
 from watchlist.tests.base import StubConfig
-
-
-class TestCurrentSubscriptionsLoader(GithubMockTestCase):
-
-    def test_returns_all_repositories_with_watching_state(self):
-        self.given_user_is_in_organisations('bigorg')
-        self.given_repositories('john.doe/foo', 'bigorg/bar')
-        self.given_subscriptions('john.doe/foo')
-
-        self.reckon_github_read_requests()
-        self.mocker.replay()
-
-        loader = CurrentSubscriptionsLoader(StubConfig())
-        self.assertEquals({'john.doe/foo': {'watching': True},
-                           'bigorg/bar': {'watching': False}},
-                          loader.load_current_subscriptions())
-
-    def test_does_not_return_watch_repositories_without_member_access(self):
-        self.given_repositories('john.doe/foo', 'otheruser/bar')
-
-        self.reckon_github_read_requests()
-        self.mocker.replay()
-
-        loader = CurrentSubscriptionsLoader(StubConfig())
-        self.assertEquals(['john.doe/foo'],
-                          loader.load_current_subscriptions().keys())
-
+from unittest2 import TestCase
+from watchlist.strategy import UpdateStrategy
 
 class TestUpdateStrategy(TestCase):
 
